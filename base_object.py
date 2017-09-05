27001dba9bad5fe21df2044eb0556812c803b48c
Allen Kennedy Jr <allen_kennedy@mcafee.com>
2017-09-05 13:07:59 -0800

    refactor: extract base object class
    All objects have a name and a description... so abstract it.

---
+++ base_object.py
@@ -0,0 +1,26 @@
+class BaseObject:
+    def __init__(self, name):
+        self._name = name
+        self._description = None
+
+    # One way to use Property
+    def set_description(self, text):
+        self._description = text
+
+    def get_description(self):
+        return self._description
+
+    description = property(get_description, set_description)
+
+    # another way to use Property
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, text):
+        self._name = text
+
+    # describe function
+    def describe(self): 
+        print( self.description )
--- room.py
+++ room.py
@@ -1,30 +1,9 @@
-class Room():
+from  base_object import BaseObject
+
+class Room(BaseObject):
     def __init__(self, name):
-        self._name = name
-        self._description = None
+        super().__init__(name)
         self.linked_rooms = {}
-
-    # One way to use Property
-    def set_description(self, text):
-        self._description = text
-
-    def get_description(self):
-        return self._description
-
-    description = property(get_description, set_description)
-
-    # another way to use Property
-    @property
-    def name(self):
-        return self._name
-
-    @name.setter
-    def name(self, text):
-        self._name = text
-
-    # describe function
-    def describe(self): 
-        print( self.description )
 
     def link_room(self, room_to_link, direction):
         self.linked_rooms[direction] = room_to_link
